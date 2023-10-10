20692fd284c281edfd1dc2cb2bc51e0a4a70bfba
Rahul Davamani <davamanirahul@gmail.com>
2023-10-10 14:08:53 +0530

    Integrated Lucia Auth

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,10 +1,24 @@
 import type { PrismaClient } from '@prisma/client';
+import type { Session } from 'lucia';
 declare global {
 	namespace App {
 		// interface Error {}
-		// interface Locals {}
+		interface Locals {
+			auth: import('lucia').AuthRequest;
+			session: Session | null;
+		}
 		// interface PageData {}
 		// interface Platform {}
+	}
+	namespace Lucia {
+		type Auth = import('$lib/server/lucia').Auth;
+		type DatabaseUserAttributes = {
+			name: string;
+			email: string;
+		};
+		type DatabaseSessionAttributes = {
+			user_picture: string;
+		};
 	}
 	// eslint-disable-next-line no-var
 	var prisma: PrismaClient;
---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,1 @@
+<a href="/login/google" class="btn">Sign in with google</a>
